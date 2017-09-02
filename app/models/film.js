e538b6ed0d032801970d13ce99864d110e433214
Armel Larcier <armel.larcier@gmail.com>
2017-09-02 20:26:45 +0200

    feat(film): add non-relational model properties

--- app/models/film.js
+++ app/models/film.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr('string')
+  title: DS.attr('string'),
+  description: DS.attr('string'),
+  director: DS.attr('string'),
+  producer: DS.attr('string'),
+  release_date: DS.attr('number'),
+  rt_score: DS.attr('number'),
+  url: DS.attr('string')
 });
