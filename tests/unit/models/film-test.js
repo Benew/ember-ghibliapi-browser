63d4e676180bd8774de2910afb2f7382d6d2672a
Armel Larcier <armel.larcier@gmail.com>
2017-09-02 20:28:54 +0200

    test(film): add test for findAll method
    Difficulties encountered while trying to cass the `findAll` method.
    `this.store().findAll('film')` would just call `/films` instead of the url defined in the app's custom adapter.
    Had to add `needs: ['adapter:application', 'serializer:application']` inside the test's configuration to make it aware of both the url and the serializer.

--- tests/unit/models/film-test.js
+++ tests/unit/models/film-test.js
@@ -2,11 +2,23 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('film', 'Unit | Model | film', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['adapter:application', 'serializer:application']
 });
 
 test('it exists', function(assert) {
   let model = this.subject();
-  // let store = this.store();
   assert.ok(!!model);
+});
+
+test('its records have `string` title', function(assert) {
+  let store = this.store();
+  const done = assert.async();
+  store.findAll('film')
+  .then(function(films){
+    films.forEach(function(film){
+      assert.ok(typeof film.get('title') === 'string');
+    });
+    done();
+  })
+  .catch(done);
 });
