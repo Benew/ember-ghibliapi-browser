8fbac73505e735567f14a48415fc9676361ccc3f
Armel Larcier <armel.larcier@gmail.com>
2017-09-09 01:14:06 +0200

    style(indent): better arrow fn indentation

--- app/routes/film.js
+++ app/routes/film.js
@@ -3,10 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     let locations = this.store.findAll('location')
-    .then(results => results.filter(location => {
-      return !!location.get('films')
-      .find(film => film.get('id') === params.film_id);
-    }));
+    .then(results =>
+      results.filter(location =>
+        !!location.get('films')
+        .find(film =>
+          film.get('id') === params.film_id
+        )
+      )
+    );
     return Ember.RSVP.hash({
       film: this.store.findRecord('film', params.film_id),
       locations
