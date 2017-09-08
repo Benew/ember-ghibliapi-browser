bddce9fce3de6f01a7c61d7abe8393087d12a30d
Armel Larcier <armel.larcier@gmail.com>
2017-09-09 01:18:02 +0200

    feat(vehicles): generate vehicle model

---
+++ app/models/vehicle.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  description: DS.attr('string'),
+  vehicleClass: DS.attr('string'),
+  length: DS.attr('string'),
+  pilot: DS.belongsTo('person'),
+  films: DS.hasMany('film')
+});
