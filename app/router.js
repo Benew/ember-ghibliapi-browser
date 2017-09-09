3ce934f7c607ca67f413a74e5e6958aa052b7a6d
Armel Larcier <armel.larcier@gmail.com>
2017-09-09 09:33:10 +0200

    feat(vehicle): generate route

--- app/router.js
+++ app/router.js
@@ -10,6 +10,7 @@ Router.map(function() {
   this.route('films');
   this.route('film', { path: '/film/:film_id' });
   this.route('person', { path: '/person/:id' });
+  this.route('vehicle');
 });
 
 export default Router;
