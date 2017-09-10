ec1e29cbaa1ec03e9eaf61348064375140861524
Armel Larcier <armel.larcier@gmail.com>
2017-09-10 16:55:48 +0200

    fix(favorites): fix component test

--- tests/integration/components/favorite-button-test.js
+++ tests/integration/components/favorite-button-test.js
@@ -11,7 +11,7 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{favorite-button}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), 'favorite_outline');
 
   // Template block usage:
   this.render(hbs`
@@ -20,5 +20,5 @@ test('it renders', function(assert) {
     {{/favorite-button}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'favorite_outline');
 });
